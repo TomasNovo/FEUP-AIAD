c66c0528b18e81f9fbdca2f5e6067e5652225384
ReleaseTheKraken <tomasnovo98@gmail.com>
2019-10-10 20:41:26 +0100

    Testing sending files

--- AgentProject/src/logic/CPU.java
+++ AgentProject/src/logic/CPU.java
@@ -1,6 +1,15 @@
 package logic;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
@@ -10,6 +19,10 @@ import jade.lang.acl.ACLMessage;
 
 public class CPU extends Agent
 {
+	int id = 0;
+	ArrayList<CompilationFile> files;
+	String projectPath;
+	
 	boolean receivedProjects;
 	String client;
 	String clientName;
@@ -31,8 +44,11 @@ public class CPU extends Agent
 	class CompilingBehaviour extends Behaviour
 	{
 		int iterationCounter = 0;
+		File f;
+		byte[] fileContent = null;
+		String clientName;
+		
 		String[] files;
-		double[] times;
 		
 		public CompilingBehaviour(String[] files)
 		{
@@ -41,45 +57,152 @@ public class CPU extends Agent
 			else
 				this.files = files;
 			
-			times = new double[this.files.length];
 		}
 		
-		@Override
-		public void action()
+		public int saveFile(String cn)
 		{
-			//System.out.println(++iterationCounter);
+			String path = "CPU-Projects/";
+			path += cn;
 			
-			//ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-//			msg.setContent("Send");
-//			msg.addReceiver(new AID("", AID.ISLOCALNAME));
-
+			File newFile = new File(path);
 			
-			/*for(int i = 0; i < files.length; i++)
+			try {
+				if(newFile.createNewFile())
+				{
+					newFile.mkdir();
+				    System.out.println("File created");
+				} else {
+				    System.out.println("File already exists.");
+				}
+			} catch (IOException e) {
+				System.out.println("ERROR: CPU: Error creating file to save");
+				e.printStackTrace();
+				return -1;
+			}
+			
+			try (FileOutputStream stream = new FileOutputStream(path)) {
+			    stream.write(fileContent);
+			} catch (FileNotFoundException e) {
+				System.out.println("ERROR: CPU: Error writing to new file");
+				e.printStackTrace();
+				return -1;
+			} catch (IOException e) {
+				System.out.println("ERROR: CPU: Error writing to new file");
+				e.printStackTrace();
+				return -1;
+			}
+			return 0;
+		}
+		
+		public int receiveFile()
+		{
+			ACLMessage msg = null;
+			
+			for (int i = 0; msg == null; i++)
 			{
-				
-			}*/
+				//System.out.println(i);
+				msg = receive();
+			}
+			if (msg != null) 
+			{
+			  String filename = msg.getUserDefinedParameter("filename");
+			  f = new File(filename);// create file called fileName
+			  fileContent = msg.getByteSequenceContent();
+
+			  return 0;
+			}
+			else return -1;
 			
-			ACLMessage msg = receive();
+		}
+		
+		public int receiveClientAID()
+		{
+			ACLMessage msg = null;
 			
-			if(msg != null)
+			for (int i = 0; msg == null; i++)
+			{
+				//System.out.println(i);
+				msg = receive();
+			}
+		
+			
+			if (msg != null)
 			{
 				String info = msg.getContent();
 				
 				clientName = info.substring(0, info.indexOf('@'));
+				System.out.println("Clientname: " + clientName);
 				clientIP = info.substring(info.indexOf('@') + 1, info.indexOf('/'));
 				
 				clientFolder = new File("../AgentProject/CPU-Projects/" + clientName).mkdirs();
 				
-				JOptionPane.showMessageDialog(null, msg.getContent());
+				System.out.println(msg.getContent());
 				
+//				JOptionPane.showMessageDialog(null, msg.getContent());	
+				return 0;
 			}
-			else block();
+			
+			return -1;
+			
+		}
+		
+		@Override
+		public void action()
+		{			
+			if(receiveClientAID() != 0) {System.out.println("ERROR: CPU: Error receiving AID"); block();}
+			if(receiveFile() != 0) {System.out.println("ERROR: CPU: Error receiving file"); return;}
+			//if(saveFile(this.clientName) != 0) {System.out.println("ERROR: CPU: Error saving file"); return;}	
+			block();
 		}
 
 		@Override
 		public boolean done()
 		{
-			return (iterationCounter == 15);
+			return true;
+		}
+		
+	}
+	
+	class ReadProject extends Behaviour
+	{
+		String path;
+	
+		public ReadProject(String path)
+		{
+			this.path = path;
+		}
+		
+		@Override
+		public void action()
+		{
+			files = new ArrayList<CompilationFile>();
+			
+	        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(path)))
+	        {
+	            for (Path child : dirStream)
+	            {
+	            	files.add(new CompilationFile(child.toFile()));
+	            }
+
+	        }
+	        catch (IOException e) 
+	        {
+	            e.printStackTrace();
+	        }
+	        
+	        for (int i = 0; i < files.size(); i++)
+	        {
+	        	files.get(i).compile();
+	        }
+	        
+	        block();
+
+		}
+
+		@Override
+		public boolean done()
+		{
+			return true;
 		}
 		
 	}
--- AgentProject/src/logic/Client.java
+++ AgentProject/src/logic/Client.java
@@ -1,5 +1,6 @@
 package logic;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -17,19 +18,21 @@ public class Client extends Agent
 	int id = 0;
 	ArrayList<CompilationFile> files;
 	String projectPath;
+	Object[] args;
+	
 	
 	@Override
 	protected void setup()
 	{
 		super.setup();
 
-		Object[] args = getArguments();
+		this.args = getArguments();
 		
 //		System.out.println(System.getProperty("user.dir"));
 
 		//addBehaviour(new ReadProject((String)args[0]));
 		
-		addBehaviour(new OfferProjectBehaviour());
+		addBehaviour(new OfferProjectBehaviour("../AgentProject/project/main.cpp"));
 		
 		System.out.println("Hey! Its me, " + getAID().getName());
 	}
@@ -37,20 +40,66 @@ public class Client extends Agent
 	class OfferProjectBehaviour extends Behaviour
 	{
 		boolean sentClient = false;
+		String filename = "";
+		
+		public OfferProjectBehaviour(String f)
+		{
+			this.filename = f;
+		}
 		
 		@Override
 		public void action()
 		{
-//			System.out.println(++iterationCounter);
+			sendClientAID();
 			
+			if(sendFileToCompile(this.filename) != 0)
+				return;
+				
+			sentClient = true;
+			
+			block();
+		}
+
+		public void sendClientAID()
+		{
 			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
 			msg.setContent(getAID().getName());
 			msg.addReceiver(new AID("CPU", AID.ISLOCALNAME));
 			send(msg);
-			sentClient = true;
-
 		}
-
+		
+		public int sendFileToCompile(String filename)
+		{
+			File f = new File(filename);
+			byte[] fileContent = null;
+			
+			if(!f.exists())
+			{
+				System.out.println("ERROR: Client: Inexistent file, please enter a valid path !" );
+				return -1;
+			}
+			
+			try 
+			{
+				fileContent = Files.readAllBytes(f.toPath());
+			} 
+			catch (IOException e) 
+			{
+				System.out.println("ERROR: Client: Error sending file" );
+				e.printStackTrace();
+				return -1;
+			}
+			
+			
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.addReceiver(new AID("CPU", AID.ISLOCALNAME));
+			msg.setByteSequenceContent(fileContent);
+			msg.addUserDefinedParameter("filename", filename);
+			send(msg);
+			
+			return 0;	
+		}
+		
 		@Override
 		public boolean done()
 		{
@@ -59,47 +108,4 @@ public class Client extends Agent
 		
 	}
 	
-	class ReadProject extends Behaviour
-	{
-		String path;
-	
-		public ReadProject(String path)
-		{
-			this.path = path;
-		}
-		
-		@Override
-		public void action()
-		{
-			files = new ArrayList<CompilationFile>();
-			
-	        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(Paths.get(path)))
-	        {
-	            for (Path child : dirStream)
-	            {
-	            	files.add(new CompilationFile(child.toFile()));
-	            }
-
-	        }
-	        catch (IOException e) 
-	        {
-	            e.printStackTrace();
-	        }
-	        
-	        for (int i = 0; i < files.size(); i++)
-	        {
-	        	files.get(i).compile();
-	        }
-	        
-	        block();
-
-		}
-
-		@Override
-		public boolean done()
-		{
-			return true;
-		}
-		
-	}
 }
