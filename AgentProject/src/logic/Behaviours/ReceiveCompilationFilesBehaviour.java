311b930edb8da6f208b5aa2282210dccf6f15320
TomasNovo <tomasnovo98@gmail.com>
2019-10-21 16:22:48 +0100

    Started to clean some code

---
+++ AgentProject/src/logic/Behaviours/ReceiveCompilationFilesBehaviour.java
@@ -0,0 +1,65 @@
+package logic.Behaviours;
+
+import java.util.ArrayList;
+
+import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+import logic.Client;
+import logic.CompilationFile;
+
+class ReceiveCompilationFilesBehaviour extends Behaviour
+{
+	ArrayList<CompilationFile> files;
+	
+	public ReceiveCompilationFilesBehaviour(ArrayList<CompilationFile> f)
+	{
+		files = f;
+	}
+	
+	@Override
+	public void action()
+	{
+		for	(int i = 0; i < files.size(); i++) // Iterates through every file to receive the binary
+		{
+			ACLMessage msg = this.myAgent.blockingReceive();
+			String filename = msg.getUserDefinedParameter("filename");
+			
+			boolean found = false;
+			
+			for	(int j = 0; j < files.size(); j++)
+			{
+				if (files.get(j).filename.equals(filename)) // Sets the binary to the respective CompilationFile
+				{
+					CompilationFile cf = files.get(i);
+					cf.binary = msg.getByteSequenceContent();
+					files.set(j, cf);
+					
+					if (!cf.saveBinary())
+					{
+						((Client) this.myAgent).errorPrintln("Failed to save binary!");
+						return;
+					}
+					
+					found = true;
+					break;
+				}
+			}
+			
+			if (!found)
+			{
+				((Client) this.myAgent).errorPrintln("Failed to find respective CompilationFile!");
+				return;
+			}
+		}
+		
+		((Client) this.myAgent).println("Successfully received compilation files");
+		
+	}
+	
+	@Override
+	public boolean done()
+	{
+		return true;
+	}
+	
+}
