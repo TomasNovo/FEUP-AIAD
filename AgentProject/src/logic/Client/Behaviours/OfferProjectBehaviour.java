8e66f04c846726a243bae2180b83084f8a7fd681
ReleaseTheKraken <tomasnovo98@gmail.com>
2019-12-09 15:55:54 +0000

    feat: changed csv

--- AgentProject/src/logic/Client/Behaviours/OfferProjectBehaviour.java
+++ AgentProject/src/logic/Client/Behaviours/OfferProjectBehaviour.java
@@ -74,9 +74,9 @@ public class OfferProjectBehaviour extends Behaviour
             }
             
             // Assign code CompilationFiles to CPUs. Only the CompilationFiles in agent.files are to be compiled (.cpp)
-//            distributeFilesEvenlyBySize();
+            distributeFilesEvenlyBySize();
 //            distributeFilesRandomly();
-            distributeFilesRandomlyBalanced();
+//            distributeFilesRandomlyBalanced();
             
     		for (int i = 0; i < infos.size(); i++)
     		{
